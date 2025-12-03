2a51bf246d81df7c6d18adc3ac776f5841ce11af
saeedjhn <saeedjahanakbari@gmail.com>
2025-12-03 22:33:31 +0330

    commit-6

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+
+const envFilePathMap = {
+  development: 'deploy/dev/.env',
+  production: 'deploy/prod/.env',
+  staging: 'deploy/staging/.env',
+};
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: envFilePathMap[process.env.NODE_ENV] ?? 'deploy/dev/.env',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
